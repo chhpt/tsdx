8b8e7ae7c148b76e0ace7d08a0e1ef47d3628b33
wuyiyang <cwuyiqing@gmail.com>
2021-04-23 01:31:25 -0700

    feat: update prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -12,20 +12,13 @@ module.exports = {
   // 对象的 key 仅在必要时用引号
   quoteProps: 'as-needed',
   // 末尾不需要逗号
-  trailingComma: 'es5',
+  trailingComma: 'all',
   // 大括号内的首尾需要空格
   bracketSpacing: true,
   // 箭头函数，只有一个参数的时候，也需要括号
   arrowParens: 'always',
-  // 每个文件格式化的范围是文件的全部内容
-  rangeStart: 0,
-  rangeEnd: Infinity,
-  // 不需要写文件开头的 @prettier
-  requirePragma: false,
-  // 不需要自动在文件开头插入 @prettier
-  insertPragma: false,
   // 使用默认的折行标准
   proseWrap: 'preserve',
-  // 换行符使用 lf
-  endOfLine: 'lf',
+  // 换行符使用 auto
+  endOfLine: 'auto',
 }
